d4c1ea6437387ef88fd76b446368f2612d174fde
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-04-28 22:03:19 +0000

    Fix 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number < 0:
 else:
     mod = number % 10
 if mod > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, mod))
+    print("Last digit of {} is {} and is greater than 5".format(number, mod), end="\n")
 elif mod == 0:
-    print("Last digit of {} is {} and is 0".format(number, mod))
+    print("Last digit of {} is {} and is 0".format(number, mod), end="\n")
 elif mod < 6 and mod != 0:
     print("Last digit of {} is {} and is less than 6 and not 0"
-          .format(number, mod))
+          .format(number, mod), end="\n")
