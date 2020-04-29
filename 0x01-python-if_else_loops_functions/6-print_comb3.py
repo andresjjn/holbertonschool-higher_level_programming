9354c1bbcf7ff93bf1b07e3292e1fdc4699d7ac4
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-04-29 00:38:05 +0000

    Fix 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 for dec in range(0, 9):
     for uni in range(1, 10):
-        print("{:d}".format(dec), end="")
-        if dec == 8 and uni == 9:
-            print("{:d}".format(uni))
-        else:
-            print("{:d}, ".format(uni), end="")
-        uni += 1
-    uni = dec + 2
-    dec += 1
+        if dec < uni:
+            print("{:d}".format(dec), end="")
+            if dec == 8 and uni == 9:
+                print("{:d}".format(uni))
+            else:
+                print("{:d}, ".format(uni), end="")
