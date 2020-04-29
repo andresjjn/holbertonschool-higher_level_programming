c938a821eb2ec8d3bfb3e4a3158275fc381c2264
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-04-29 01:33:56 +0000

    Fix 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for num in range(0, 90):
     if num != 89 and num / 10 < num % 10:
         print("{:02d}".format(num), end=", ")
     elif num / 10 < num % 10:
-        print("89")
+        print("{:02d}".format(num))
