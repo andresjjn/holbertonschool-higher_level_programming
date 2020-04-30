5797ddc902f6d6af7d03ca4f16a9e3f3aeb50ab9
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-04-30 23:37:41 +0000

    Add 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    test_list = dir(hidden_4)
+    start_letter = "_"
+    without_ = \
+        list(filter(lambda x: not x.startswith(start_letter), test_list))
+    without_.sort()
+    for lis in range(0, 3):
+        print("{}".format(without_[lis]))
