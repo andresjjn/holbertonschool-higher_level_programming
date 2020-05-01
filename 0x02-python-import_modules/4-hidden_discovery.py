adddd7361be290a09a14c8bf6afcbce5dd1ce8bb
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-01 01:36:55 +0000

    Fix: 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    test_list = dir(hidden_4)
-    start_letter = "_"
-    without_ = \
-        list(filter(lambda x: not x.startswith(start_letter), test_list))
-    for lis in range(0, 3):
-        print("{}".format(without_[lis]))
+    i = dir(hidden_4)
+    for lis in range(0, len(i)):
+        if i[lis][0] != "_":
+            print("{}".format(i[lis]))
