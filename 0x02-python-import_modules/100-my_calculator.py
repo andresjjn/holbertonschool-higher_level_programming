8ec62ca7cc9bd91d354295393e7d0143c5a1dcf8
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-01 02:43:00 +0000

    Fix: 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     a = len(argv)
     if a != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print(a)
         exit(1)
     if argv[2] not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
