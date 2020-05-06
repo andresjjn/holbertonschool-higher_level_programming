b1e5f267b7759db5084d63791babcd9e1c1f0c4e
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-06 03:44:07 +0000

    Add: 11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        if idx < 0 or idx > len(my_list) - 1:
+            return my_list
+        tmp = []
+        for i in range(0, idx):
+            tmp.append(my_list[i])
+        for j in range(idx + 1, len(my_list)):
+            tmp.append(my_list[j])
+        my_list[:] = list(tmp)
+        return my_list
