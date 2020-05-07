a21854fb3faca9d7dfb2401305e228419c950351
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-07 22:10:20 +0000

    Fix: 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     add = my_list[0]
-    my_list.sort()
-    for i in range(len(my_list) - 1):
-        if (my_list[i] != my_list[i + 1]):
-            add += my_list[i + 1]
+    my_list_cpy = my_list.copy()
+    my_list_cpy.sort()
+    for i in range(len(my_list_cpy) - 1):
+        if (my_list_cpy[i] != my_list_cpy[i + 1]):
+            add += my_list_cpy[i + 1]
     return add
