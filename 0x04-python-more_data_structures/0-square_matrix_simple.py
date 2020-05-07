d76d848e97528544f261dd14ce31447ca3c0ffdc
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-07 16:05:51 +0000

    Add: README.md and 0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[
+        0 for row in range(len(matrix))] for col in range(len(matrix[0]))]
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+                new_matrix[row][col] = matrix[row][col] ** 2
+    return new_matrix
