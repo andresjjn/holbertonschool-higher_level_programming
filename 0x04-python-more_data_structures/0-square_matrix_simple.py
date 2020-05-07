ca37198c8034a6d6896cfca6ef70f869b9279c6b
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-07 19:42:00 +0000

    Fix: 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        new_matrix = [[
-            0 for row in range(len(matrix))] for col in range(len(matrix[0]))]
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                new_matrix[row][col] = matrix[row][col] ** 2
-        return new_matrix
+    new_matrix = [[
+        0 for row in range(len(matrix))] for col in range(len(matrix[0]))]
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            new_matrix[row][col] = matrix[row][col] ** 2
+    return new_matrix
