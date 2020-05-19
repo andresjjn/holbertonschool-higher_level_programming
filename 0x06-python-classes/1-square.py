f3df61d3f697ecd6fd18e93765f06404a4687fd6
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-19 23:23:46 +0000

    Add: 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empy class square that define a square"""
+
+
+class Square:
+    """Empy class"""
+    def __init__(self, size):
+        """Declaration of filds"""
+        self.__size = size
