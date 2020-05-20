5433fd6e417314828c8c4e92d78876f8ea10d7e4
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-20 00:01:33 +0000

    Add: 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,7 +19,7 @@ class Square:
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
-    
+
     def area(self):
         """Calculate the area of a square
         Returns:
