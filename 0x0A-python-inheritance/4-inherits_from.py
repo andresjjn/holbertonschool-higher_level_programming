5b6a7c21ecd94ac7a70dd54dada61408a8705947
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-02 23:30:02 +0000

    Fix: 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This file containsunction that returns True if the object 
+"""This file containsunction that returns True if the object
 is an instance of a class that inherited (directly or indirectly) from the
 specified class; otherwise False
 """
