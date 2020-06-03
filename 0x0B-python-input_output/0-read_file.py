4a022694d630dfc3036100c578bad6c9043beb92
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-03 16:41:38 +0000

    Add: README.md 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This file containt a funtion to read a file and print
+the result string in stdout"""
+
+
+def read_file(filename=""):
+    """Funtion read and print
+    Args: Name of file to read
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
