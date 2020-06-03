4400885864f2e94cdd5d592e6d8dc02e077a4541
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-03 03:33:08 +0000

    Add: 7-base_geometry.py and tests/7-base_geometry.txt

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This file contains class named based Geomertry
+"""
+
+
+class BaseGeometry:
+    """TClass base geometry
+    """
+
+    def area(self):
+        """Area public instane method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validator public instance method"""
+        if not isinstance(value, int):
+            raise TypeError("%s must be an integer" % name)
+        if value <= 0:
+            raise ValueError("%s must be greater than 0" % name)
