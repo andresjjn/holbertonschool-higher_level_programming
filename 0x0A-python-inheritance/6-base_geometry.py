c734741108aac5b24e46f2c514921110df042501
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-03 02:53:52 +0000

    Add: 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This file contains class named based Geomertry
+"""
+
+
+class BaseGeometry:
+    """TClass base geometry
+    """
+
+    def area(self):
+        """Area public instane method"""
+        raise Exception("area() is not implemented")
