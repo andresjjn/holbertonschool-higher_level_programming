600b41e1f5dc64972d527fc9227176de81793a8c
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-03 22:23:17 +0000

    Fix: 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     Args: Name of file to read
     """
     with open(filename, mode='r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
