cddc5e46c6302d6d0e36e1fc85e0bc2cba7995f9
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-04 04:08:17 +0000

    Add: 12-student.py

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This file containt a class named Student"""
+
+
+class Student:
+    """Class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Special method to init intances"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Function that returns the dictionary description with simple data
+        structure"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        b = dict()
+        if not attrs:
+            return self.__dict__
+        elif type(attrs) == list:
+            for i in attrs:
+                for an in self.__dict__:
+                    if i == an:
+                        b.update({i: self.__dict__[i]})
+        return b
