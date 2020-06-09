dde0454f9d496ab21e62e4a531460b595614574c
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-09 02:25:38 +0000

    Update: test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,11 +2,20 @@
 """Unittest for Base class from almost circle python project
 """
 import unittest
+import pep8
 from models.base import Base
 
 
 class Test_base(unittest.TestCase):
     """Class test using unittest"""
+    
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
+
     def test_Base(self):
         """Test constructor"""
         b1 = Base()
