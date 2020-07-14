5fe268a2af90a32170fef9d440324de313ce6f8c
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-07-14 20:31:25 +0000

    Add: 12-no_genre.sql and hbtn_0d_tvshows.sql Update: 10-genre_id_by_show.sql and 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- Script that lists all cities contained in the database hbtn_0d_usa.
+-- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT title, genre_id FROM tv_shows, tv_show_genres
 WHERE  tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
