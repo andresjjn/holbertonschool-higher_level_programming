1b690808e52391a1e212923aeaa39d47d81a3a77
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-08-08 15:23:59 +0000

    Add: 0-select_states.py, 0-select_states.sql and README.md

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
@@ -8,7 +9,9 @@ def mysqlconnect():
     password = sys.argv[2]
     database = sys.argv[3]
     try:
-        connection = MySQLdb.connect("localhost", username, password, database)
+        connection = MySQLdb.connect(host="localhost", port=3306,
+                                     user=username, passwd=password,
+                                     db=database)
     except:
         print("Can't connect to database")
         return
