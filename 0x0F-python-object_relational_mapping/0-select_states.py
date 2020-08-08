f69746405f1779e23d5f016f90128ae5a455da4b
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-08-08 15:08:18 +0000

    Add: 0-select_states.py, 0-select_states.sql and README.md

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def mysqlconnect():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    try:
+        connection = MySQLdb.connect("localhost", username, password, database)
+    except:
+        print("Can't connect to database")
+        return
+    cursor = connection.cursor()
+    cursor.execute("SELECT states.id, states.name FROM
+                   states ORDER BY states.id ASC")
+    records = cursor.fetchall()
+    for row in records:
+        print("({}, '{}')".format(row[0], row[1]))
+    cursor.close()
+    connection.close()
+
+mysqlconnect()
