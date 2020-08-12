127c1bf8e781e5c0a529f0e395e45c1e8a1234cb
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-08-12 02:18:56 +0000

    Add: 100-relationship_states_cities.py, relationship_city.py and relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class definition of a State and an instance Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+Base = declarative_base()
+
+
+class State(Base):
+    """Class State with args"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
