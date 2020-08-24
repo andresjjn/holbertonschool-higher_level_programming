46f3abf06c535fd300fd6a0137b5ab7568b7ad2b
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-08-24 20:45:35 +0000

    Fix: 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,4 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     res = response.read()
     cod = res.decode('utf-8')
     print("Body response:\n\ttype: {}\n\tcontent: {}\n\tutf8 content: {}\
-          ".format(res, type(res), cod))
+          ".format(type(res), res, cod))
