84599a81b7b69fbc8c6221bc594ec1e85d5b511b
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-08-25 01:37:15 +0000

    Add: 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that takes in a URL, sends a request to the
+URL and displays the body of the response."""
+from sys import argv
+import requests
+
+
+if len(argv) is 2:
+    try:
+        res = requests.post(argv[1])
+        if res.status_code >= 400:
+            print("Error code: {}".format(res.status_code))
+    except:
+        pass
