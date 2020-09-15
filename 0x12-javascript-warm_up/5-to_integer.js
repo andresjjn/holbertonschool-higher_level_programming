24627fc8d00935e7bd2e2bf7a50d8115da88fb7f
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-15 17:38:57 +0000

    Fix: 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const integer = parseInt(process.argv[2]);
 if (isNaN(integer)) {
   console.log('Not a number');
 } else {
-  console.log(`My number is: ${integer}`);
+  console.log(`My number: ${integer}`);
 }
