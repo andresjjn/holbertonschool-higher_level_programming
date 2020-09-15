0854a64ca982cc5b3df940e407d5d93c2df38729
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-15 17:40:40 +0000

    Fix: 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Perro', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let index = 0; index < array.length; index++) {
   console.log(array[index]);
 }
