51180e4e8745654c4424567b12946fdabd984f65
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-21 18:13:55 +0000

    Fix: Return list in 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
   if (list !== undefined && searchElement !== undefined) {
-    const aCount = new Map([...new Set(list)].map(x => [x, list.filter(y => y === x).length]));
-    const lists = [aCount.get(searchElement)];
-    return (lists);
+    for (let index = 0; index < list.length; index++) {
+      if (list[index] === searchElement) {
+        counter += 1;
+      }
+    }
+    return (counter);
   }
   return (undefined);
 };
