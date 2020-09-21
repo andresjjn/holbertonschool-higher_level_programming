455e84a3b9a1eb566ab231bb51ab03235e241118
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-21 18:28:30 +0000

    Add: 8-main.js and 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const lists = [];
-  for (let index = list.length - 1; index >= 0 ; index--) {
+  for (let index = list.length - 1; index >= 0; index--) {
     lists.push(list[index]);
   }
   return (lists);
