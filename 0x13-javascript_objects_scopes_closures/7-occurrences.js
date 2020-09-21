08d10b075a0dc650554fcfbbaecc76377e6afe38
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-21 17:49:55 +0000

    Add: 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  const aCount = new Map([...new Set(list)].map(x => [x, list.filter(y => y === x).length]));
-  return (aCount.get(searchElement));
+  if (list !== undefined && searchElement !== undefined) {
+    const aCount = new Map([...new Set(list)].map(x => [x, list.filter(y => y === x).length]));
+    return (aCount.get(searchElement));
+  }
+  return (undefined);
 };
