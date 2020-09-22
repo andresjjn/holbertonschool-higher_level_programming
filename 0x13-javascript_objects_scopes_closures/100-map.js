8c7138819504a3bbd8ea50b15eaf92ec1fc4a232
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-22 02:49:58 +0000

    Fix: 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-main').list;
-const mapping = list.map((current, index) => { return current * index; });
-console.log(list);
+const lists = require('./100-main').list;
+const mapping = lists.map((current, index) => { return current * index; });
+console.log(lists);
 console.log(mapping);
