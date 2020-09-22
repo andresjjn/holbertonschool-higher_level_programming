8b7f7e696cb7360a6f2eeb077e73e485e7961721
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-22 17:13:29 +0000

    Fix: if non args

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,7 @@
 const file = require('fs');
 const path = process.argv[2];
 const message = process.argv[3];
-if (!path || !message) {
-  process.exit();
-}
-file.appendFile(path, message, 'utf-8', (err, data) => {
+file.appendFile(path, message, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
